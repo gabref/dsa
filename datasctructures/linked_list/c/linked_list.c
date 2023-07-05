1c17ec94517ca00a5094d3950ebe88d6d4832b7b
gabref <gabrielfranzer@gmail.com>
2023-07-05 08:26:33 -0300

    implemented more functions

--- datasctructures/linked_list/c/linked_list.c
+++ datasctructures/linked_list/c/linked_list.c
@@ -65,6 +65,89 @@ void gll_push_end(gnode ** head, int value) {
     }
 }
 
+int gll_pop_beg(gnode ** head) {
+    if (*head == NULL) {
+        fprintf(stderr, "LinkedList is empty\n");
+        exit(EXIT_FAILURE);
+    }
+    gnode *temp = *head;
+    *head = temp->next;
+    int popped_value = temp->data;
+    free(temp);
+    return popped_value;
+}
+
+int gll_pop_end(gnode ** head) {
+    if (*head == NULL) {
+        fprintf(stderr, "LinkedList is empty\n");
+        exit(EXIT_FAILURE);
+    }
+    gnode *current = *head;
+    gnode *prev = NULL;
+    while(current->next != NULL) {
+        prev = current;
+        current = current->next;
+    }
+
+    int popped_value = current->data;
+
+    // linkedlist has only 1 element
+    if (prev == NULL) *head = NULL;
+    else prev->next = NULL;
+
+    free(current);
+    return popped_value;
+}
+
+int gll_get_beg(gnode ** head) {
+    if (*head == NULL) {
+        fprintf(stderr, "LinkedList is empty\n");
+        exit(EXIT_FAILURE);
+    }
+    gnode *current = *head;
+    return current->data;
+}
+
+int gll_get_end(gnode ** head) {
+    if (*head == NULL) {
+        fprintf(stderr, "LinkedList is empty\n");
+        exit(EXIT_FAILURE);
+    }
+    gnode *current = *head;
+
+    while(current->next)
+        current = current->next;
+    return current->data;
+}
+
+void gll_insert(gnode ** head, int index, int value) {
+    if (index < 0) {
+        fprintf(stderr, "Index out of bound\n");
+        exit(EXIT_FAILURE);
+    }
+    gnode *current = *head;
+    for (int i = 0; i < index - 1; i++) {
+        if (current->next == NULL) {
+            fprintf(stderr, "Index out of bound\n");
+            exit(EXIT_FAILURE);
+        }
+        current = current->next;
+    }
+
+    gnode *temp = (gnode *) malloc(sizeof(gnode));
+    check_address(temp);
+
+    temp->data = value;
+    
+    if (current == NULL || current->next == NULL) {
+        temp->next = current;
+        *head = temp;
+    } else {
+        temp->next = current->next;
+        current->next = temp;
+    }
+}
+
 void gll_print(gnode ** head) {
     printf("\n\n*** LinkedList ***\n");
     for (gnode *current = *head; current != NULL; current = current->next)
--- datasctructures/linked_list/c/test.c
+++ datasctructures/linked_list/c/test.c
@@ -12,6 +12,11 @@ void run_all_tests() {
     test_size();
     test_is_empty();
     test_at();
+    test_pop_beg();
+    test_pop_end();
+    test_get_beg();
+    test_get_end();
+    test_insert();
 
     printf("\n\nAll tests ok\n\n");
 }
@@ -51,6 +56,78 @@ void test_push_end() {
     assert(myLL->data == 10);
     assert(myLL->next->data == 20);
     assert(gll_at(&myLL, 3) == 40);
+
+    gll_destroy(&myLL);
+}
+
+void test_pop_beg() {
+    printf("Testing pop begin\n");
+
+    gnode *myLL = gll_create();
+    for (int i = 0; i < 4; i++)
+        gll_push_beg(&myLL, i * 10);
+
+    assert(gll_pop_beg(&myLL) == 30);
+    assert(gll_pop_beg(&myLL) == 20);
+    assert(gll_pop_beg(&myLL) == 10);
+    assert(gll_pop_beg(&myLL) == 0);
+
+    gll_destroy(&myLL);
+}
+
+void test_pop_end() {
+    printf("Testing pop end\n");
+
+    gnode *myLL = gll_create();
+    for (int i = 0; i < 4; i++)
+        gll_push_beg(&myLL, i * 10);
+
+    assert(gll_pop_end(&myLL) == 0);
+    assert(gll_pop_end(&myLL) == 10);
+    assert(gll_pop_end(&myLL) == 20);
+    assert(gll_pop_end(&myLL) == 30);
+
+    gll_destroy(&myLL);
+}
+
+void test_get_beg() {
+    printf("Testing get begin\n");
+
+    gnode *myLL = gll_create();
+    for (int i = 0; i < 4; i++)
+        gll_push_beg(&myLL, i * 10);
+
+    assert(gll_get_beg(&myLL) == 30);
+
+    gll_destroy(&myLL);
+}
+
+void test_get_end() {
+    printf("Testing get end\n");
+
+    gnode *myLL = gll_create();
+    for (int i = 0; i < 4; i++)
+        gll_push_beg(&myLL, i * 10);
+
+    assert(gll_get_end(&myLL) == 0);
+
+    gll_destroy(&myLL);
+}
+
+void test_insert() {
+    printf("Testing get insert\n");
+
+    gnode *myLL = gll_create();
+
+    gll_insert(&myLL, 0, 7);
+    gll_insert(&myLL, 0, 4);
+    gll_insert(&myLL, 1, 5);
+    gll_insert(&myLL, 2, 6);
+
+    assert(gll_at(&myLL, 0) == 4);
+    assert(gll_at(&myLL, 1) == 5);
+    assert(gll_at(&myLL, 2) == 6);
+    assert(gll_at(&myLL, 3) == 7);
 
     gll_destroy(&myLL);
 }
